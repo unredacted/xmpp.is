5e755b510c61a2c7a71df32852ac27eb7e7517a5
Lunar <lunar@cypherpunk.is>
2017-07-19 21:55:39 -0500

    Create delete-prosody-offline-messages.sh

---
+++ scripts/delete-prosody-offline-messages.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+find /var/lib/prosody/xmpp%2eis/offline -name "*.list" -size +1M -delete
+find /var/lib/prosody/greysec%2enet/offline -name "*.list" -size +1M -delete
+find /var/lib/prosody/y2qmqomqpszzryei%2eonion/offline -name "*.list" -size +1M -delete
