bfa89fb25b6aa5084752831dce9a01c39dc74e8f
Lunar <lunar@cypherpunk.is>
2017-07-21 02:14:21 -0500

    Update letsencrypt-to-hiawatha.sh

--- scripts/letsencrypt-to-hiawatha.sh
+++ scripts/letsencrypt-to-hiawatha.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 # This script is executed from the systemd Let's Encrypt renewal service to ensure the updated certs are in place and converted to Hiawatha format
 
-# xmpp.is
+# xmpp.is, xmpp.co, xmpp.cx, xmpp.xyz
 cat /etc/letsencrypt/live/xmpp.is/privkey.pem /etc/letsencrypt/live/xmpp.is/fullchain.pem > /etc/hiawatha/ssl/xmpp.is.pem
+cat /etc/letsencrypt/live/xmpp.co/privkey.pem /etc/letsencrypt/live/xmpp.co/fullchain.pem > /etc/hiawatha/ssl/xmpp.co.pem
+cat /etc/letsencrypt/live/xmpp.cx/privkey.pem /etc/letsencrypt/live/xmpp.cx/fullchain.pem > /etc/hiawatha/ssl/xmpp.cx.pem
+cat /etc/letsencrypt/live/xmpp.xyz/privkey.pem /etc/letsencrypt/live/xmpp.xyz/fullchain.pem > /etc/hiawatha/ssl/xmpp.xyz.pem
 
 # webstats.xmpp.is
 cat /etc/letsencrypt/live/webstats.xmpp.is/privkey.pem /etc/letsencrypt/live/webstats.xmpp.is/fullchain.pem > /etc/hiawatha/ssl/webstats.xmpp.is.pem
-
-# xmpp.co
-cat /etc/letsencrypt/live/xmpp.co/privkey.pem /etc/letsencrypt/live/xmpp.co/fullchain.pem > /etc/hiawatha/ssl/xmpp.co.pem
-
-# xmpp.cx
-cat /etc/letsencrypt/live/xmpp.cx/privkey.pem /etc/letsencrypt/live/xmpp.cx/fullchain.pem > /etc/hiawatha/ssl/xmpp.cx.pem
-
-# xmpp.xyz
-cat /etc/letsencrypt/live/xmpp.xyz/privkey.pem /etc/letsencrypt/live/xmpp.xyz/fullchain.pem > /etc/hiawatha/ssl/xmpp.xyz.pem
