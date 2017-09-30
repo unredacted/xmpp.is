87a3e5aa5f15b7ef8fcc0fb92625e3c0c65d10e7
Lunar <lunar@cypherpunk.is>
2017-09-30 17:16:06 -0500

    Create letsencrypt-to-hiawatha.sh

---
+++ scripts/letsencrypt-to-hiawatha.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR1="xmpp.is"
+DIR2="xmpp.co"
+DIR3="xmpp.cx"
+DIR4="xmpp.xyz"
+LE_DIR="/etc/letsencrypt/live/"
+HIAWATHA_CERTS="/etc/hiawatha/ssl/"
+HIAWATHA="/etc/hiawatha/"
+
+cat "${LE_DIR}${DIR1}"/privkey.pem "${LE_DIR}${DIR1}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR1}".pem
+cat "${LE_DIR}${DIR2}"/privkey.pem "${LE_DIR}${DIR2}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR2}".pem
+cat "${LE_DIR}${DIR3}"/privkey.pem "${LE_DIR}${DIR3}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR3}".pem
+cat "${LE_DIR}${DIR4}"/privkey.pem "${LE_DIR}${DIR4}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR4}".pem
+
+# webstats.xmpp.is
+cat /etc/letsencrypt/live/webstats.xmpp.is/privkey.pem /etc/letsencrypt/live/webstats.xmpp.is/fullchain.pem > /etc/hiawatha/ssl/webstats.xmpp.is.pem
+
+# Restart Hiawatha
+service hiawatha restart
