d3e6b5fd6285d7c9ac27ccfbe53d057c16bba740
Lunar <lunar@cypherpunk.is>
2017-09-30 14:40:32 -0500

    Create xmpp.cx.cfg.lua

---
+++ conf.d/xmpp.cx.cfg.lua
@@ -0,0 +1,40 @@
+VirtualHost "xmpp.cx"
+http_host = "http.xmpp.cx"
+modules_enabled = { "http", "register_web" };
+reload_modules = { "tls", "http", "register_web", "http_upload" };
+
+	      enabled = true
+        allow_registration = false;
+	      min_seconds_between_registrations = 300
+
+	      c2s_require_encryption = true
+	      s2s_secure_auth = false
+
+	      authentication = "internal_hashed"
+        storage = "internal"
+
+	ssl = {
+	   certificate = "/etc/prosody/certs/xmpp.cx/fullchain.pem";
+	   key = "/etc/prosody/certs/xmpp.cx/privkey.pem";
+}
+
+        https_ssl = {
+           certificate = "/etc/prosody/certs/xmpp.cx/fullchain.pem";
+           key = "/etc/prosody/certs/xmpp.cx/privkey.pem";
+}
+
+        http_ports = { 5280 }
+        http_interfaces = { "*" }
+
+        https_ports = { 5281 }
+        https_interfaces = { "*" }
+
+	      register_web_template = "/etc/prosody/register-templates/Prosody-Web-Registration-Theme"
+
+        Component "upload.xmpp.cx" "http_upload"
+        http_upload_file_size_limit = 52428800
+        http_upload_expire_after = 60 * 60 * 24 * 7
+
+        Component "muc.xmpp.is" "muc"
+        name = "XMPP.is MUC"
+        restrict_room_creation = "local"
