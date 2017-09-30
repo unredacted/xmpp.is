4af19468c2ee2305c33a4f145cf007a5428f037e
Lunar <lunar@cypherpunk.is>
2017-09-30 14:51:43 -0500

    Update y2qmqomqpszzryei.onion.cfg.lua

--- conf.d/y2qmqomqpszzryei.onion.cfg.lua
+++ conf.d/y2qmqomqpszzryei.onion.cfg.lua
@@ -6,15 +6,15 @@ reload_modules = { "tls", "onions" };
   
 	allow_registration = true;
   
-  c2s_require_encryption = true
+        c2s_require_encryption = true
 	s2s_secure_auth = false
 
 	authentication = "internal_hashed"
-  storage = "internal"
+        storage = "internal"
 
 	ssl = {
-		certificate = "/etc/prosody/certs/xmpp.is/fullchain.pem";
-		key = "/etc/prosody/certs/xmpp.is/privkey.pem";
+	   certificate = "/etc/prosody/certs/xmpp.is/fullchain.pem";
+	   key = "/etc/prosody/certs/xmpp.is/privkey.pem";
 }
         
   onions_tor_all =  true 
@@ -22,13 +22,13 @@ reload_modules = { "tls", "onions" };
   
   onions_map = {
   ["jabber.calyxinstitute.org"] = "ijeeynrc6x2uy5ob.onion";
-	["jabber.ccc.de"] = "okj7xc6j2szr2y75.onion";
-	["jabber.ipredator.se"] = "3iffdebkzzkpgipa.onion";
+  ["jabber.ccc.de"] = "okj7xc6j2szr2y75.onion";
+  ["jabber.ipredator.se"] = "3iffdebkzzkpgipa.onion";
   ["jabber.otr.im"] = "5rgdtlawqkcplz75.onion";
-	["jabber.systemli.org"] = "x5tno6mwkncu5m3h.onion";
-	["jabber.so36.net"] = "s4fgy24e2b5weqdb.onion";
-	["jabber.cryptoparty.is"] = "cryjabkbdljzohnp.onion";
-	["jabber.lqdn.fr"] = "jabber63t4r2qi57.onion";
+  ["jabber.systemli.org"] = "x5tno6mwkncu5m3h.onion";
+  ["jabber.so36.net"] = "s4fgy24e2b5weqdb.onion";
+  ["jabber.cryptoparty.is"] = "cryjabkbdljzohnp.onion";
+  ["jabber.lqdn.fr"] = "jabber63t4r2qi57.onion";
   ["xmpp.riseup.net"] = "4cjw6cwpeaeppfqz.onion";
   ["xmpp.rows.io"] = "yz6yiv2hxyagvwy6.onion";
   ["autistici.org"] = "wi7qkxyrdpu5cmvr.onion";
@@ -36,13 +36,13 @@ reload_modules = { "tls", "onions" };
   ["daemons.cf"] = "daemon4jidu2oig6.onion";
   ["otromundo.cf"] = "arauemwe2utqqzye.onion";
   ["taolo.ga"] = "l3ybpw4vs6ie5rv2.onion";
-	["securejabber.me"] = "giyvshdnojeivkom.onion";
-	["cloak.dk"] = "m2dsl4banuimpm6c.onion";
-	["kode.im"] = "ihkw7qy3tok45dun.onion";
-	["im.koderoot.net"] = "ihkw7qy3tok45dun.onion";
-	["wtfismyip.com"] = "ofkztxcohimx34la.onion";
-	["dukgo.com"] = "wlcpmruglhxp6quz.onion";
-	["patchcord.be"] = "xsydhi3dnbjuatpz.onion";
+  ["securejabber.me"] = "giyvshdnojeivkom.onion";
+  ["cloak.dk"] = "m2dsl4banuimpm6c.onion";
+  ["kode.im"] = "ihkw7qy3tok45dun.onion";
+  ["im.koderoot.net"] = "ihkw7qy3tok45dun.onion";
+  ["wtfismyip.com"] = "ofkztxcohimx34la.onion";
+  ["dukgo.com"] = "wlcpmruglhxp6quz.onion";
+  ["patchcord.be"] = "xsydhi3dnbjuatpz.onion";
 }
 
 -- Private stuff injected below after git pull --
