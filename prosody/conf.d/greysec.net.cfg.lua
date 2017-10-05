c3de9e228511e0f7bf325dc494a8fdb1c7c97fab
Lunar <lunar@cypherpunk.is>
2017-10-05 10:46:53 -0500

    Create greysec.net.cfg.lua

---
+++ prosody/conf.d/greysec.net.cfg.lua
@@ -0,0 +1,16 @@
+VirtualHost "greysec.net"
+admins = { "lunar@greysec.net", "insider@greysec.net" }
+
+	enabled = true
+        allow_registration = false;
+
+	c2s_require_encryption = true
+	s2s_secure_auth = false
+
+	authentication = "internal_hashed"
+        storage = "internal"
+
+	ssl = {
+		certificate = "/etc/prosody/certs/greysec.net.crt";
+		key = "/etc/prosody/certs/greysec.net.key";
+}
