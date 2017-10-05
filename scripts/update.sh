aab63888459027d575ad1ff4b480810b5b42f3e5
Lunar <lunar@cypherpunk.is>
2017-10-05 10:52:40 -0500

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,7 @@ echo
 
 # Git
 cd /home/git/xmpp.is && git pull
-cd /etc/prosody/modules/git-modules/mod_email_pass_reset && git pull
+cd /etc/prosody/modules/git-modules/mod_email_pass_reset_english && git pull
 cd /etc/prosody/register-templates/Prosody-Web-Registration-Theme && git pull
 
 # Mercurial
