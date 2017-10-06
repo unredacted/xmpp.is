7f159c600588dd9162f1dac0178c5326a29e45f3
Lunar <lunar@cypherpunk.is>
2017-10-06 02:30:18 -0500

    Create sync.sh

---
+++ scripts/sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Prosody config
+rsync -av /home/git/xmpp.is/etc/prosody/ /etc/prosody/
+
+# Hiawatha config
+rsync -av /home/git/xmpp.is/etc/hiawatha/ /etc/hiawatha/
+
+# Tor config
+rsync -av /home/git/xmpp.is/etc/tor/ /etc/tor/
+
+# Webroot
+rsync -av /home/git/xmpp.is/var/www/ /var/www/
+
+# Cron
+crontab /home/git/xmpp.is/var/spool/cron/crontabs/root
+cp /home/git/xmpp.is/etc/cron.d/certbot /etc/cron.d/certbot
