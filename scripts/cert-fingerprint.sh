dd5065b630258543d9483e5592136f8a8f742f5f
Lunar <lunar@cypherpunk.is>
2017-10-06 23:30:21 -0500

    Update cert-fingerprint.sh

--- scripts/cert-fingerprint.sh
+++ scripts/cert-fingerprint.sh
@@ -9,6 +9,9 @@ DIR4="xmpp.xyz"
 LE_DIR="/etc/letsencrypt/live"
 OUTPUT_DIR="/var/www/transparency.xmpp.is"
 
+# Remove old file
+rm -f /var/www/transparency.xmpp.is/fingerprints.txt
+
 echo "XMPP.is Fingerprints" | tee -a "${OUTPUT_DIR}"/fingerprints.txt
 openssl x509 -in "${LE_DIR}/${DIR1}"/cert.pem -noout -sha1 -fingerprint >> "${OUTPUT_DIR}"/fingerprints.txt
 openssl x509 -in "${LE_DIR}/${DIR1}"/cert.pem -noout -sha256 -fingerprint >> "${OUTPUT_DIR}"/fingerprints.txt
