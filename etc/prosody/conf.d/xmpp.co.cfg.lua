f5cc3356e6d982cbca9d9be2138bd00efeaea755
Lunar <lunar@cypherpunk.is>
2017-10-16 11:52:29 -0500

    Update xmpp.co.cfg.lua

--- etc/prosody/conf.d/xmpp.co.cfg.lua
+++ etc/prosody/conf.d/xmpp.co.cfg.lua
@@ -30,7 +30,7 @@ reload_modules = { "tls", "http", "register_web", "http_upload" };
         https_ports = { 5281 }
         https_interfaces = { "*" }
 
-	register_web_template = "/etc/prosody/register-templates/Prosody-Web-Registration-Theme"
+	register_web_template = "/etc/prosody/register-templates/prosody_web_registration_theme"
 
         Component "upload.xmpp.co" "http_upload"
         http_upload_file_size_limit = 10485760
