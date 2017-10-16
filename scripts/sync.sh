3d933cffb966502403f78d43c769df8110fdc96f
Lunar <lunar@cypherpunk.is>
2017-10-16 12:01:37 -0500

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -15,6 +15,14 @@ rsync -av /home/git/xmpp.is/var/www/ /var/www/
 # Mercurial
 rsync -av /home/git/xmpp.is/etc/mercurial/ /etc/mercurial/
 
+# Prosody Modules
+
+# prosody_web_registration_theme
+rsync -av /home/git/prosody_web_registration_theme /etc/prosody/register-templates/
+
+# mod_email_pass_reset_english
+rsync -av /home/git/mod_email_pass_reset_english /var/lib/prosody/modules/
+
 # Cron
 crontab /home/git/xmpp.is/var/spool/cron/crontabs/root
 cp /home/git/xmpp.is/etc/cron.d/certbot /etc/cron.d/certbot
