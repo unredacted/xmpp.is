4bb2e6c1e2da2078d76b376f2c804ff6433e9a0e
Lunar <lunar@cypherpunk.is>
2017-10-24 07:20:53 -0500

    Update connection-stats.sh

--- scripts/connection-stats.sh
+++ scripts/connection-stats.sh
@@ -5,7 +5,7 @@ OUTPUT="/var/www/transparency.xmpp.is/connection-stats.txt"
 { echo "c2s:show()"; sleep 1; } | telnet localhost 5582 | grep -a "Total:" > "${OUTPUT}"
 
 sed -i 's/| OK: Total: //g' "${OUTPUT}"
-sed -i 's/clients/C2S connections/g'
+sed -i 's/clients/C2S connections/g' "${OUTPUT}"
 
 sed -i '/connections/i \
 Currently serving:' "${OUTPUT}"
