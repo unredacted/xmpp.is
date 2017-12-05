415dbdfcbcc5500985768e463244e5b1d133eb78
Lunar <lunar@cypherpunk.is>
2017-12-05 09:40:03 -0600

    Update delete-prosody-offline-messages.sh

--- scripts/delete-prosody-offline-messages.sh
+++ scripts/delete-prosody-offline-messages.sh
@@ -6,4 +6,5 @@ find /var/lib/prosody/xmpp%2eis/offline -name "*.list" -size +1M -delete
 find /var/lib/prosody/xmpp%2eco/offline -name "*.list" -size +1M -delete
 find /var/lib/prosody/xmpp%2ecx/offline -name "*.list" -size +1M -delete
 find /var/lib/prosody/xmpp%2exyz/offline -name "*.list" -size +1M -delete
+find /var/lib/prosody/xmpp%2efi/offline -name "*.list" -size +1M -delete
 find /var/lib/prosody/y2qmqomqpszzryei%2eonion/offline -name "*.list" -size +1M -delete
