0d347f7110a01a843c22b54cfc1baf6ec996f89e
Lunar <lunar@cypherpunk.is>
2017-12-09 13:13:51 -0600

    Update cleanup-prosody-offline-messages.sh

--- scripts/cleanup-prosody-offline-messages.sh
+++ scripts/cleanup-prosody-offline-messages.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-# This script is used to find and delete offline messages larger than 1MB to prevent storage DOS
+# This script is used to find and delete offline messages larger than 1MB to prevent storage DoS
 
 find /var/lib/prosody/xmpp%2eis/offline -name "*.list" -size +1M -delete
 find /var/lib/prosody/xmpp%2eco/offline -name "*.list" -size +1M -delete
