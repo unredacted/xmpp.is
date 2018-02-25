ede8cb3948bc9317b3a8c1ed853e17a522fdf8ae
Lunar <lunar@cypherpunk.is>
2018-02-24 21:16:22 -0600

    Create anti-bruteforce.sh

---
+++ scripts/anti-bruteforce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script prevents massive bruteforce attacks against accounts
+
+# Find and sort IP addresses making many C2S connections without being authenticated
+{ echo "c2s:show()"; sleep 1; } | telnet localhost 5582 | grep -a c2s_unauthed | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep -v '144.76.47.248' | grep -v 127.0.0.1 | awk '{print $1}' | cut -d: -f1 | sort | uniq -c | sort -n > /tmp/excess_c2s_connections.txt
+
+# Sort IP addresses and output
+cat /tmp/excess_c2s_connections.txt | awk '$1 > 50  {print}' | awk '{print $2}' > /tmp/sorted_excess_c2s_connections.txt
+
+# Drop connections from sorted IP list
+cat /tmp/sorted_excess_c2s_connections.txt | awk '{gsub("IP:", "");print}' | while read IP
+do
+  iptables -A INPUT -s $IP -j DROP
+done
+
+# Sleep for 5 minutes and remove blocks
+sleep 5m; cat /tmp/sorted_excess_c2s_connections.txt | awk '{gsub("IP:", "");print}' | while read IP
+do
+  iptables -D INPUT -s $IP -j DROP
+done
