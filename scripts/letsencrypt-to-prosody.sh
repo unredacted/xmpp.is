619f57a5f0672cf9091e4d5190031ef833544ff2
Lunar <lunar@cypherpunk.is>
2018-03-23 18:52:27 -0500

    Update letsencrypt-to-prosody.sh

--- scripts/letsencrypt-to-prosody.sh
+++ scripts/letsencrypt-to-prosody.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 # Script that copies over all new certificates to Prosody and sets correct permissions
 
-DIR1="xmpp.is"
-DIR2="xmpp.co"
-DIR3="xmpp.cx"
-DIR4="xmpp.xyz"
-DIR5="xmpp.fi"
-LE_DIR="/etc/letsencrypt/live/"
-CERTS="/etc/prosody/certs/"
+LE_DIR="/home/user/git/dehydrated/certs/all/"
+PROSODY_CERTS="/etc/prosody/certs/"
 PROSODY="/etc/prosody/"
 
-cp -rfL "${LE_DIR}${DIR1}/" "${CERTS}"
-cp -rfL "${LE_DIR}${DIR2}/" "${CERTS}"
-cp -rfL "${LE_DIR}${DIR3}/" "${CERTS}"
-cp -rfL "${LE_DIR}${DIR4}/" "${CERTS}"
-cp -rfL "${LE_DIR}${DIR5}/" "${CERTS}"
+cp "${LE_DIR}.fullchain.pem" "${PROSODY_CERTS}"
+cp "${LE_DIR}.privkey.pem" "${PROSODY_CERTS}"
 
 chown -R prosody:prosody "${PROSODY}"
 chmod -R 700 "${CERTS}"
