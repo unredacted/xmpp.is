954537eb64c2bb4943d5ae19c4d34058d5d6cb3e
Lunar <lunar@cypherpunk.is>
2018-03-23 18:41:40 -0500

    Testing dehydrated

--- scripts/letsencrypt-to-hiawatha.sh
+++ scripts/letsencrypt-to-hiawatha.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 # Script that copies all new certificates over and puts them in Hiawatha format
 
-DIR1="xmpp.is"
-DIR2="xmpp.co"
-DIR3="xmpp.cx"
-DIR4="xmpp.xyz"
-DIR5="xmpp.fi"
-LE_DIR="/etc/letsencrypt/live/"
+LE_DIR="/home/user/git/dehydrated/certs/all/"
 HIAWATHA_CERTS="/etc/hiawatha/ssl/"
-HIAWATHA="/etc/hiawatha/"
 
-cat "${LE_DIR}${DIR1}"/privkey.pem "${LE_DIR}${DIR1}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR1}".pem
-cat "${LE_DIR}${DIR2}"/privkey.pem "${LE_DIR}${DIR2}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR2}".pem
-cat "${LE_DIR}${DIR3}"/privkey.pem "${LE_DIR}${DIR3}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR3}".pem
-cat "${LE_DIR}${DIR4}"/privkey.pem "${LE_DIR}${DIR4}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR4}".pem
-cat "${LE_DIR}${DIR5}"/privkey.pem "${LE_DIR}${DIR5}"/fullchain.pem > "${HIAWATHA_CERTS}${DIR5}".pem
+cat "${LE_DIR}"/privkey.pem "${LE_DIR}"/fullchain.pem > "${HIAWATHA_CERTS}"hiawatha.pem
 
 # Restart Hiawatha
 service hiawatha restart
