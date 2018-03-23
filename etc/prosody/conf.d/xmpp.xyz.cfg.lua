d2ca040eef6df94864cfd47753b4b0fab3346ed5
Lunar <lunar@cypherpunk.is>
2018-03-23 18:56:44 -0500

    Update xmpp.xyz.cfg.lua

--- etc/prosody/conf.d/xmpp.xyz.cfg.lua
+++ etc/prosody/conf.d/xmpp.xyz.cfg.lua
@@ -16,13 +16,8 @@ reload_modules = { "tls", "onions", "http", "register_web", "email_pass_reset_en
         storage = "internal"
 
 	ssl = {
-	   certificate = "/etc/prosody/certs/xmpp.xyz/fullchain.pem";
-	   key = "/etc/prosody/certs/xmpp.xyz/privkey.pem";
-}
-
-        https_ssl = {
-           certificate = "/etc/prosody/certs/xmpp.xyz/fullchain.pem";
-           key = "/etc/prosody/certs/xmpp.xyz/privkey.pem";
+	   certificate = "/etc/prosody/certs/fullchain.pem";
+	   key = "/etc/prosody/certs/privkey.pem";
 }
 
         http_ports = { 5280 }
