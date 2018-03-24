9bc387544b1c8cc75f87af5afce733293dc531a8
Lunar <lunar@cypherpunk.is>
2018-03-24 12:33:55 -0500

    Update le-renew-hook.sh

--- scripts/le-renew-hook.sh
+++ scripts/le-renew-hook.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This script is ran after certbot renews a certificate, links to other scripts that keep certs automagically up-to-date
 
-bash /home/user/git/xmpp.is/scripts/letsencrypt-to-hiawatha.sh
-bash /home/user/git/xmpp.is/scripts/letsencrypt-to-prosody.sh
-bash /home/user/git/xmpp.is/scripts/cert-fingerprint.sh
+GIT_DIR="/home/user/git"
+
+bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-hiawatha.sh
+bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-prosody.sh
+bash "${GIT_DIR}"/xmpp.is/scripts/cert-fingerprint.sh
