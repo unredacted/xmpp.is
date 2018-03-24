3e28e44c1e61a6f61f5e2872507bc3a8f028134d
Lunar <lunar@cypherpunk.is>
2018-03-23 19:14:54 -0500

    Update letsencrypt-to-prosody.sh

--- scripts/letsencrypt-to-prosody.sh
+++ scripts/letsencrypt-to-prosody.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Script that copies over all new certificates to Prosody and sets correct permissions
 
-LE_DIR="/home/user/git/dehydrated/certs/all/"
-PROSODY_CERTS="/etc/prosody/certs/"
-PROSODY="/etc/prosody/"
+LE_DIR="/home/user/git/dehydrated/certs/all"
+PROSODY_CERTS="/etc/prosody/certs"
+PROSODY="/etc/prosody"
 
-cp "${LE_DIR}fullchain.pem" "${PROSODY_CERTS}"
-cp "${LE_DIR}privkey.pem" "${PROSODY_CERTS}"
+cp "${LE_DIR}"/fullchain.pem "${PROSODY_CERTS}"
+cp "${LE_DIR}"/privkey.pem "${PROSODY_CERTS}"
 
 chown -R prosody:prosody "${PROSODY}"
-chmod -R 700 "${CERTS}"
+chmod -R 700 "${PROSODY_CERTS}"
 prosodyctl reload
 
 # Make sure mod_tls and mod_http get reloaded
