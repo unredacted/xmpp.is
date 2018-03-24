ec6e64003b5381c86aa558ba2bfb06b3e0ff8dab
Lunar <lunar@cypherpunk.is>
2018-03-23 19:13:08 -0500

    Update letsencrypt-to-hiawatha.sh

--- scripts/letsencrypt-to-hiawatha.sh
+++ scripts/letsencrypt-to-hiawatha.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Script that copies all new certificates over and puts them in Hiawatha format
 
-LE_DIR="/home/user/git/dehydrated/certs/all/"
-HIAWATHA_CERTS="/etc/hiawatha/ssl/"
+LE_DIR="/home/user/git/dehydrated/certs/all"
+HIAWATHA_CERTS="/etc/hiawatha/ssl"
 
-cat "${LE_DIR}"/privkey.pem "${LE_DIR}"/fullchain.pem > "${HIAWATHA_CERTS}"hiawatha.pem
+cat "${LE_DIR}"/privkey.pem "${LE_DIR}"/fullchain.pem > "${HIAWATHA_CERTS}"/hiawatha.pem
 
 # Restart Hiawatha
 service hiawatha restart
