b771a1e1a88b2057dc53d190627b2d64dd6721c0
Lunar <lunar@cypherpunk.is>
2018-03-23 19:19:51 -0500

    Update cert-fingerprint.sh

--- scripts/cert-fingerprint.sh
+++ scripts/cert-fingerprint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 # Script to output the fingerprints of currently installed certificates
 
-S1="xmpp.is"
-S2="xmpp.co"
-S3="xmpp.cx"
-S4="xmpp.xyz"
-S5="xmpp.fi"
-HS1="y2qmqomqpszzryei.onion"
-LE_DIR="/etc/letsencrypt/live"
+LE_DIR="/home/user/git/dehydrated/certs/all"
 PROSODY_DIR="/etc/prosody/certs"
+HS1="y2qmqomqpszzryei.onion"
 TEMP_OUTPUT_FILE="/tmp/fingerprints.txt"
 OUTPUT_FILE="/var/www/transparency.xmpp.is/fingerprints.txt"
 
@@ -20,33 +15,9 @@ echo Certificate fingerprints since `date` | tee -a "${TEMP_OUTPUT_FILE}"
 
 echo | tee -a "${TEMP_OUTPUT_FILE}"
 
-echo "XMPP.is Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S1}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S1}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-
-echo | tee -a "${TEMP_OUTPUT_FILE}"
-
-echo "XMPP.co Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S2}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S2}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-
-echo | tee -a "${TEMP_OUTPUT_FILE}"
-
-echo "XMPP.cx Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S3}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S3}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-
-echo | tee -a "${TEMP_OUTPUT_FILE}"
-
-echo "XMPP.xyz Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S4}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S4}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-
-echo | tee -a "${TEMP_OUTPUT_FILE}"
-
-echo "XMPP.fi Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S5}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
-openssl x509 -in "${LE_DIR}/${S5}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
+echo "XMPP.* Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
+openssl x509 -in "${LE_DIR}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
+openssl x509 -in "${LE_DIR}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
 
 echo | tee -a "${TEMP_OUTPUT_FILE}"
 
