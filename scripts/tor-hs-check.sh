65e7aa0dd543b864e0b971d9fffec698bbaa668f
Lunar <lunar@cypherpunk.is>
2018-04-14 18:40:55 -0500

    Update tor-hs-check.sh

--- scripts/tor-hs-check.sh
+++ scripts/tor-hs-check.sh
@@ -5,12 +5,12 @@ if torsocks curl --connect-timeout 30 y2qmqomqpszzryei.onion:5222/ | grep "xml" 
   then
     echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 was reachable!" > /dev/null
   else
-    echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 is unreachable!" | mail -s "Tor HSv2 Down" admin@lunar.systems
+    echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 is unreachable!" | mail -s "Tor HSv2 Down" cron@lunar.systems
 fi
 
 if torsocks curl --connect-timeout 30 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222/ | grep "xml" | grep "stream" > /dev/null
   then
     echo "The Tor HSv3 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222 is reachable!" > /dev/null
   else
-    echo "The Tor HSv3 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222 is unreachable!" | mail -s "Tor HSv3 Down" admin@lunar.systems
+    echo "The Tor HSv3 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222 is unreachable!" | mail -s "Tor HSv3 Down" cron@lunar.systems
 fi
