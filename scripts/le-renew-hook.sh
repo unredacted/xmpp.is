44b353c61a7c8b7c7cc0fd4ae1f0f08df3c632d1
Lunar <lunar@cypherpunk.is>
2018-04-27 22:36:41 -0500

    Fix for LE cert renewal

--- scripts/le-renew-hook.sh
+++ scripts/le-renew-hook.sh
@@ -3,6 +3,17 @@
 
 GIT_DIR="/home/user/git"
 
-bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-hiawatha.sh
-bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-prosody.sh
-bash "${GIT_DIR}"/xmpp.is/scripts/cert-fingerprint.sh
+if grep "1" /tmp/dehydrated-flag
+  then
+
+    bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-hiawatha.sh
+    bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-prosody.sh
+    bash "${GIT_DIR}"/xmpp.is/scripts/cert-fingerprint.sh
+
+    echo "Script run complete! Set flag back to 0"
+    echo "0" > /tmp/dehydrated-flag
+
+  else
+    echo "Looks like the flag is set to 0... Exiting now!"
+    exit
+  fi
