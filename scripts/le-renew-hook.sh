785f76fd256350eee50b21f0b3ec37db6a1ab987
Lunar <lunar@cypherpunk.is>
2018-05-11 14:05:55 -0500

    Update le-renew-hook.sh

--- scripts/le-renew-hook.sh
+++ scripts/le-renew-hook.sh
@@ -4,16 +4,19 @@
 GIT_DIR="/home/user/git"
 
 if grep "1" /tmp/dehydrated-flag
+
   then
 
     bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-hiawatha.sh
     bash "${GIT_DIR}"/xmpp.is/scripts/letsencrypt-to-prosody.sh
     bash "${GIT_DIR}"/xmpp.is/scripts/cert-fingerprint.sh
 
-    echo "Script run complete! Set flag back to 0"
+    echo "Script run complete! Set flag back to 0."
     echo "0" > /tmp/dehydrated-flag
 
   else
+  
     echo "Looks like the flag is set to 0... Exiting now!"
     exit
+    
   fi
