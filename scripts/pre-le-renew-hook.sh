8e0d0a060f73bdc5d13a20ae57006c0b2b63311f
Lunar <lunar@cypherpunk.is>
2018-06-25 00:03:15 -0500

    Update pre-le-renew-hook.sh

--- scripts/pre-le-renew-hook.sh
+++ scripts/pre-le-renew-hook.sh
@@ -2,16 +2,11 @@
 # This script runs after a dehydrated cert renewal, it sets a flag for le-renew-hook.sh to check for
 
 GIT_DIR="/home/user/git"
+DEHYDRATED_RENEW_FLAG="/tmp/flags/dehydrated-renew"
 
-if find "${GIT_DIR}"/dehydrated/certs/all -cmin -30 | grep ".pem"
-  
-  then
-
-    echo "1" > /tmp/dehydrated-flag
-  
-  else
-  
-    echo "Couldn't find any .pem files created within 30 minutes. Exiting now!"
-    exit
-    
+if find "${GIT_DIR}"/dehydrated/certs/all -cmin -30 | grep ".pem"; then
+  echo "1" > "${DEHYDRATED_RENEW_FLAG}"
+else
+  echo "Couldn't find any .pem files created within 30 minutes. Exiting now!"
+  exit  
 fi
