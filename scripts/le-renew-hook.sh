d952583077da424f57f9163bc6ebea4fd581541e
Lunar <lunar@cypherpunk.is>
2018-06-25 00:00:34 -0500

    Update le-renew-hook.sh

--- scripts/le-renew-hook.sh
+++ scripts/le-renew-hook.sh
@@ -2,9 +2,10 @@
 # This script is ran after dehydrated renews a certificate, links to other scripts that keep certs automagically up-to-date
 
 GIT_DIR="/home/user/git"
+DEHYDRATED_RENEW_FLAG="/tmp/flags/dehydrated-renew"
 echo `date`
 
-if grep "1" /tmp/dehydrated-flag
+if grep "1" "${DEHYDRATED_RENEW_FLAG}"
 
   then
 
@@ -13,7 +14,7 @@ if grep "1" /tmp/dehydrated-flag
     bash "${GIT_DIR}"/xmpp.is/scripts/cert-fingerprint.sh
 
     echo "Script run complete! Set flag back to 0."
-    echo "0" > /tmp/dehydrated-flag
+    echo "0" > "${DEHYDRATED_RENEW_FLAG}"
 
   else
   
