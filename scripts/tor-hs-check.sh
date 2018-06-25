d2bc98ce68123de11b4dc2d5e7ea81578c9f1c58
Lunar <lunar@cypherpunk.is>
2018-06-24 23:55:11 -0500

    Update tor-hs-check.sh

--- scripts/tor-hs-check.sh
+++ scripts/tor-hs-check.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Script to check in the Tor Hidden Services are reachable
 
-if torsocks curl --connect-timeout 30 y2qmqomqpszzryei.onion:5222/ | grep "xml" | grep "stream" > /dev/null
+if torsocks curl --max-time 30 y2qmqomqpszzryei.onion:5222/ | grep "xml" | grep "stream" > /dev/null
   then
     echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 was reachable!" > /dev/null
   else
     echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 is unreachable!" | mail -s "Tor HSv2 Down" cron@lunar.systems
 fi
 
-if torsocks curl --connect-timeout 30 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222/ | grep "xml" | grep "stream" > /dev/null
+if torsocks curl --max-time 30 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222/ | grep "xml" | grep "stream" > /dev/null
   then
     echo "The Tor HSv3 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222 is reachable!" > /dev/null
   else
