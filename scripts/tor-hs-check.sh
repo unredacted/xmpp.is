ae70e9fde274ffdee74e47ff7b2e0959a1663bad
Lunar <lunar@cypherpunk.is>
2018-06-25 00:08:19 -0500

    Update tor-hs-check.sh

--- scripts/tor-hs-check.sh
+++ scripts/tor-hs-check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Script to check in the Tor Hidden Services are reachable
 
+TOR_RESTART_FLAG="/tmp/flags/tor-restart"
+
 if torsocks curl --max-time 30 y2qmqomqpszzryei.onion:5222/ | grep "xml" | grep "stream" > /dev/null
   then
     echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 was reachable!" > /dev/null
@@ -16,5 +18,5 @@ if torsocks curl --max-time 30 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl
 fi
 
 if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" /var/log/tor/notice.log | grep "No more HSDir available to query"; then
-  echo "1" > /tmp/flags/tor-restart
+  echo "1" > "${TOR_RESTART_FLAG}"
 fi
