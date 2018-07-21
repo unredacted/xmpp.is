19af54ebe037df2ddb2b2b541645d57793e1e95e
Lunar <lunar@cypherpunk.is>
2018-07-21 13:23:48 -0500

    Add some echos

--- scripts/tor-hs-check.sh
+++ scripts/tor-hs-check.sh
@@ -6,12 +6,15 @@ HSV3="6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion"
 HSV2="y2qmqomqpszzryei.onion"
 
 function check_logs {
+echo "Parsing logs now!"
 if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" /var/log/tor/notice.log | grep "No more HSDir available to query"; then
   echo "1" > "${TOR_RESTART_FLAG}"
 fi
+echo "Done parsing logs!"
 }
 
 function curl_test {
+echo "Checking HSv2"
 if torsocks curl --max-time 30 "${HSV2}":5222/ | grep "xml" | grep "stream" > /dev/null
   then
     echo "The Tor HSv2 "${HSV2}":5222 was reachable!" > /dev/null
@@ -19,6 +22,7 @@ if torsocks curl --max-time 30 "${HSV2}":5222/ | grep "xml" | grep "stream" > /d
     echo "The Tor HSv2 "${HSV2}":5222 is unreachable!" | mail -s "Tor HSv2 Down" cron@lunar.systems
 fi
 
+echo "Checking HSv3"
 if torsocks curl --max-time 30 "${HSV3}":5222/ | grep "xml" | grep "stream" > /dev/null
   then
     echo "The Tor HSv3 "${HSV3}":5222 is reachable!" > /dev/null
@@ -32,12 +36,12 @@ do
 case "$1" in
 
   --curl | -c)
-	curl_test
-	;;
+  curl_test
+  ;;
 
-	--check-logs | -l)
-	check_logs
-	;;
+  --check-logs | -l)
+  check_logs
+  ;;
 
   *)
   echo "Unknown parameter detected: ${1}" >&2
