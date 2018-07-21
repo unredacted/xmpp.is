b4e8b64a7783eaf780a45e7d6ae82efc0734e24c
Lunar <lunar@cypherpunk.is>
2018-07-21 13:16:29 -0500

    Make whole new script with flags

--- scripts/tor-hs-check.sh
+++ scripts/tor-hs-check.sh
@@ -2,21 +2,48 @@
 # Script to check in the Tor Hidden Services are reachable
 
 TOR_RESTART_FLAG="/home/user/flags/tor-restart"
+HSV3="6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion"
+HSV2="y2qmqomqpszzryei.onion"
 
-if torsocks curl --max-time 30 y2qmqomqpszzryei.onion:5222/ | grep "xml" | grep "stream" > /dev/null
-  then
-    echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 was reachable!" > /dev/null
-  else
-    echo "The Tor HSv2 y2qmqomqpszzryei.onion:5222 is unreachable!" | mail -s "Tor HSv2 Down" cron@lunar.systems
-fi
-
-if torsocks curl --max-time 30 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222/ | grep "xml" | grep "stream" > /dev/null
-  then
-    echo "The Tor HSv3 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222 is reachable!" > /dev/null
-  else
-    echo "The Tor HSv3 6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion:5222 is unreachable!" | mail -s "Tor HSv3 Down" cron@lunar.systems
-fi
-
+function check_logs {
 if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" /var/log/tor/notice.log | grep "No more HSDir available to query"; then
   echo "1" > "${TOR_RESTART_FLAG}"
 fi
+}
+
+function curl_test {
+if torsocks curl --max-time 30 "${HSV2}":5222/ | grep "xml" | grep "stream" > /dev/null
+  then
+    echo "The Tor HSv2 "${HSV2}":5222 was reachable!" > /dev/null
+  else
+    echo "The Tor HSv2 "${HSV2}":5222 is unreachable!" | mail -s "Tor HSv2 Down" cron@lunar.systems
+fi
+
+if torsocks curl --max-time 30 "${HSV3}":5222/ | grep "xml" | grep "stream" > /dev/null
+  then
+    echo "The Tor HSv3 "${HSV3}":5222 is reachable!" > /dev/null
+  else
+    echo "The Tor HSv3 "${HSV3}":5222 is unreachable!" | mail -s "Tor HSv3 Down" cron@lunar.systems
+fi
+}
+
+while [ ! $# -eq 0 ]
+do
+case "$1" in
+
+  --curl | -c)
+	curl_test
+	;;
+
+	--check-logs | -l)
+	check_logs
+	;;
+
+  *)
+  echo "Unknown parameter detected: ${1}" >&2
+  exit 1
+  ;;
+
+esac
+shift
+done
