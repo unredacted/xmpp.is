6d70c97355bae20870f61e50fc2db035e8fce8e5
Lunar <lunar@cypherpunk.is>
2018-07-21 13:42:33 -0500

    Update tor-hs-check.sh

--- scripts/tor-hs-check.sh
+++ scripts/tor-hs-check.sh
@@ -6,26 +6,26 @@ HSV3="6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion"
 HSV2="y2qmqomqpszzryei.onion"
 
 function check_logs {
-echo "Parsing logs now!"
+echo "Parsing logs now!" > /dev/null 2>&1
 if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" /var/log/tor/notice.log | grep "No more HSDir available to query"; then
   echo "1" > "${TOR_RESTART_FLAG}"
 fi
-echo "Done parsing logs!"
+echo "Done parsing logs!" > /dev/null 2>&1
 }
 
 function curl_test {
 echo "Checking HSv2"
-if torsocks curl --max-time 30 "${HSV2}":5222/ | grep "xml" | grep "stream" > /dev/null
+if torsocks curl --max-time 30 "${HSV2}":5222/ | grep "xml" | grep "stream" > /dev/null 2>&1
   then
-    echo "The Tor HSv2 "${HSV2}":5222 was reachable!" > /dev/null
+    echo "The Tor HSv2 "${HSV2}":5222 was reachable!" > /dev/null 2>&1
   else
     echo "The Tor HSv2 "${HSV2}":5222 is unreachable!" | mail -s "Tor HSv2 Down" cron@lunar.systems
 fi
 
 echo "Checking HSv3"
-if torsocks curl --max-time 30 "${HSV3}":5222/ | grep "xml" | grep "stream" > /dev/null
+if torsocks curl --max-time 30 "${HSV3}":5222/ | grep "xml" | grep "stream" > /dev/null 2>&1
   then
-    echo "The Tor HSv3 "${HSV3}":5222 is reachable!" > /dev/null
+    echo "The Tor HSv3 "${HSV3}":5222 is reachable!" > /dev/null 2>&1
   else
     echo "The Tor HSv3 "${HSV3}":5222 is unreachable!" | mail -s "Tor HSv3 Down" cron@lunar.systems
 fi
