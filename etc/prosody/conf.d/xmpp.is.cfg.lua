0e093d83bc22d7bf7b99f3a36b5124cb282241a9
lunarthegrey <lunar@cypherpunk.is>
2018-08-05 00:17:58 -0500

    Force secure S2S

--- etc/prosody/conf.d/xmpp.is.cfg.lua
+++ etc/prosody/conf.d/xmpp.is.cfg.lua
@@ -5,30 +5,30 @@ reload_modules = { "tls", "onions", "http", "register_web", "email_pass_reset_en
 
         enabled = true
 
-        allow_registration = false;
+	allow_registration = false;
 	min_seconds_between_registrations = 300
-        welcome_message = "Welcome to $host, make sure you browse around the site for more details about us! https://xmpp.is/"
+	welcome_message = "Welcome to $host, make sure you browse around the site for more details about us! https://xmpp.is/"
 
 	c2s_require_encryption = true
-	s2s_secure_auth = false
+	s2s_secure_auth = true
 
 	authentication = "internal_hashed"
-        storage = "internal"
+	storage = "internal"
 
 	ssl = {
 	   certificate = "/etc/prosody/certs/fullchain.pem";
 	   key = "/etc/prosody/certs/privkey.pem";
-}
+	}
 
         register_web_template = "/etc/prosody/register-templates/prosody_web_registration_theme"
 
 	--INSERT_SECRETS--
 
-        Component "upload.xmpp.is" "http_upload"
-        http_upload_expire_after = 60 * 60 * 24 * 7
-        http_upload_file_size_limit = 10000000
+	Component "upload.xmpp.is" "http_upload"
+	http_upload_expire_after = 60 * 60 * 24 * 7
+	http_upload_file_size_limit = 10000000
 	http_upload_quota = 1000000000
 
-        Component "muc.xmpp.is" "muc"
-        name = "XMPP.is MUC"
-        restrict_room_creation = "local"
+	Component "muc.xmpp.is" "muc"
+	name = "XMPP.is MUC"
+	restrict_room_creation = "local"
