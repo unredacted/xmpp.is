54a30298eb5112d51cdd16b98003d3e0adddca50
Lunar <lunar@cypherpunk.is>
2018-09-28 09:36:32 -0500

    Improve tor_curl_test function

--- scripts/service-checks.sh
+++ scripts/service-checks.sh
@@ -10,6 +10,7 @@ TOR_RESTART_FLAG="/home/user/flags/tor-restart"
 TOR_NOTICE_LOG="/var/log/tor/notice.log"
 HSV3="6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion"
 HSV2="y2qmqomqpszzryei.onion"
+LOG_ATTRIBUTE_HSDIR1="No more HSDir available to query"
 
 # Check NFS mount
 function nfs_check_mount {
@@ -23,8 +24,19 @@ fi
 # Checks Tor log for common errors, and sets flag if needed
 function tor_check_logs {
 echo "Parsing logs now!"
-if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" "${TOR_NOTICE_LOG}" | grep "No more HSDir available to query"; then
+if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" "${TOR_NOTICE_LOG}" | grep "${HSV2}" | grep "${LOG_ATTRIBUTE_HSDIR1}"; then
+  echo "The '"${LOG_ATTRIBUTE_HSDIR1}"' attribute was found in the log for "${HSV2}""
+  echo ""${HSV2}" is having issues, setting flag to 1"
   echo "1" > "${TOR_RESTART_FLAG}"
+else
+  echo "The '"${LOG_ATTRIBUTE_HSDIR1}"' attribute was not found in the log for "${HSV2}""
+fi
+if sed -n "/^$(date --date='5 minutes ago' '+%b %_d %H')/,\$p" "${TOR_NOTICE_LOG}" | grep "${HSV3}" | grep "${LOG_ATTRIBUTE_HSDIR1}"; then
+  echo "The '"${LOG_ATTRIBUTE_HSDIR1}"' attribute was found in the log for "${HSV3}""
+  echo ""${HSV3}" is having issues, setting flag to 1"
+  echo "1" > "${TOR_RESTART_FLAG}"
+else
+  echo "The '"${LOG_ATTRIBUTE_HSDIR1}"' attribute was not found in the log for "${HSV3}""
 fi
 echo "Done parsing logs!"
 }
