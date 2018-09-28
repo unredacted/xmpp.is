458c4b1b4e1ed218c1b8e94fce4cea27bf04fff4
Lunar <lunar@cypherpunk.is>
2018-09-27 22:35:06 -0500

    Add function and flag

--- scripts/service-actions.sh
+++ scripts/service-actions.sh
@@ -2,15 +2,43 @@
 # Script that checks flags for service actions
 
 TOR_RESTART_FLAG="/home/user/flags/tor-restart"
+TOR_CONFIG_PATH="/etc/tor/torrc"
 
-# Tor service actions
+# Tor restart action
+function tor_restart {
 if grep "1" "${TOR_RESTART_FLAG}"; then
   service tor stop
-  sleep 10
-  service tor start
-  echo "Tor has been restarted, setting flag back to 0!"
-  echo "0" > "${TOR_RESTART_FLAG}"
 else
   echo "The flag is set to 0, exiting now!"
   exit
 fi
+
+sleep 10
+
+if ps aux | grep -v grep | grep "${TOR_CONFIG_PATH}"; then
+  echo "Tor is still running, no need to start it!"
+  exit
+else
+  service tor start
+  echo "Tor has been restarted, setting flag back to 0!"
+  echo "0" > "${TOR_RESTART_FLAG}"
+fi
+}
+
+# Magic code for flags
+while [ ! $# -eq 0 ]
+do
+case "$1" in
+
+  --tor-restart)
+  tor_restart
+  ;;
+
+  *)
+  echo "Unknown parameter detected: ${1}" >&2
+  exit 1
+  ;;
+
+esac
+shift
+done
