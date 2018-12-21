4a6dc619837e3eb7bf553865bb49d4837b51ea92
lunarthegrey <lunar@cypherpunk.is>
2018-12-21 10:33:10 -0600

    Set archive expiry to 1 hour for initial testing

--- etc/prosody/prosody.cfg.lua
+++ etc/prosody/prosody.cfg.lua
@@ -159,7 +159,7 @@ limit_auth_max = 5
 
 -- mod_mam --
 default_archive_policy = true
-archive_expires_after = "1d"
+archive_expires_after = 60 * 60
 max_archive_query_results = 20;
 mam_smart_enable = true
 
