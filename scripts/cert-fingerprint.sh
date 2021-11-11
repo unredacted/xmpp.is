0474faf5a4c64c94348c8dddbb5e6b16b30fd412
Lunar <lunar@cypherpunk.is>
2021-11-10 20:11:15 -0600

    Add fingerprints for Tor HSv3

--- scripts/cert-fingerprint.sh
+++ scripts/cert-fingerprint.sh
@@ -4,6 +4,7 @@
 LE_DIR="/home/user/git/dehydrated/certs/all"
 PROSODY_DIR="/etc/prosody/certs"
 HS1="y2qmqomqpszzryei.onion"
+HS2="6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion"
 TEMP_OUTPUT_FILE="/tmp/fingerprints.txt"
 OUTPUT_FILE="/var/www/transparency.xmpp.is/fingerprints.txt"
 
@@ -26,6 +27,11 @@ echo | tee -a "${TEMP_OUTPUT_FILE}"
 echo "y2qmqomqpszzryei.onion Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
 openssl x509 -in "${PROSODY_DIR}/${HS1}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
 openssl x509 -in "${PROSODY_DIR}/${HS1}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
+
+echo "6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion Fingerprints" | tee -a "${TEMP_OUTPUT_FILE}"
+openssl x509 -in "${PROSODY_DIR}/${HS2}"/cert.pem -noout -sha1 -fingerprint >> "${TEMP_OUTPUT_FILE}"
+openssl x509 -in "${PROSODY_DIR}/${HS2}"/cert.pem -noout -sha256 -fingerprint >> "${TEMP_OUTPUT_FILE}"
+
 
 # Copy temp file to actual file
 cp "${TEMP_OUTPUT_FILE}" "${OUTPUT_FILE}"
